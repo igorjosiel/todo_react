398ce39eeb1222c987e6f87fafb0c5cb61b589f2
igorjosiel <igorjosielvitalino@gmail.com>
2026-05-23 02:17:23 -0300

    feat: cria o componente de texto para o projeto.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import Text from "./components/Text";
+
 export default function App() {
   return (
-    <div className="text-green-base">
-      Ola mundo
+    <div className="flex flex-col gap-2">
+      <Text variant="body-sm-bold" className="text-pink-base">Olá Mundo!</Text>
+      <Text className="text-green-base">Olá Mundo!</Text>
+      <Text variant="body-md-bold">Olá Mundo!</Text>
     </div>
-  )
+  );
 }
