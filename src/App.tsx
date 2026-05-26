43f1eb256b21f63d0e9971f5fb98c96bf421b40b
igorjosiel <igorjosielvitalino@gmail.com>
2026-05-26 00:38:19 -0300

    feat: cria o componente de ícones e utiliza o plugin vite-plugin-svgr.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import Text from "./components/Text";
+import TrashIcon from "./assets/icons/trash.svg?react";
+import CheckIcon from "./assets/icons/check.svg?react";
+import PencilIcon from "./assets/icons/pencil.svg?react";
+import PlusIcon from "./assets/icons/plus.svg?react";
+import SpinnerIcon from "./assets/icons/spinner.svg?react";
+import XIcon from "./assets/icons/x.svg?react";
+import Icon from "./components/Icon";
 
 export default function App() {
   return (
-    <div className="flex flex-col gap-2">
-      <Text variant="body-sm-bold" className="text-pink-base">Olá Mundo!</Text>
-      <Text className="text-green-base">Olá Mundo!</Text>
-      <Text variant="body-md-bold">Olá Mundo!</Text>
+    <div>
+      <div className="flex flex-col gap-2">
+        <Text variant="body-sm-bold" className="text-pink-base">Olá Mundo!</Text>
+        <Text className="text-green-base">Olá Mundo!</Text>
+        <Text variant="body-md-bold">Olá Mundo!</Text>
+      </div>
+
+      <div className="flex gap-1">
+        <Icon svg={TrashIcon} className="fill-pink-base" />
+        <Icon svg={CheckIcon} />
+        <Icon svg={PlusIcon} />
+        <Icon svg={SpinnerIcon} animate />
+        <Icon svg={PencilIcon} />
+        <Icon svg={XIcon} />
+      </div>
     </div>
   );
 }
