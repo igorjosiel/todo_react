fc220b4222d2d6e7b2bef7f48b1e7cb6c1f3bc85
igorjosiel <igorjosielvitalino@gmail.com>
2026-05-26 01:01:09 -0300

    feat: cria o componente de Badges.

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import PlusIcon from "./assets/icons/plus.svg?react";
 import SpinnerIcon from "./assets/icons/spinner.svg?react";
 import XIcon from "./assets/icons/x.svg?react";
 import Icon from "./components/Icon";
+import Badge from "./components/Badge";
 
 export default function App() {
   return (
@@ -23,6 +24,11 @@ export default function App() {
         <Icon svg={SpinnerIcon} animate />
         <Icon svg={PencilIcon} />
         <Icon svg={XIcon} />
+      </div>
+
+      <div>
+        <Badge variant="secondary">5</Badge>
+        <Badge variant="primary">2 de 5</Badge>
       </div>
     </div>
   );
