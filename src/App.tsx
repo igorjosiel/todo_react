ce804d981e31ec14713b532cf620a38c374773ba
igorjosiel <igorjosielvitalino@gmail.com>
2026-06-11 11:16:00 -0300

    feat: cria componente de card para a aplicação.

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import Button from "./components/Button";
 import ButtonIcon from "./components/ButtonIcon";
 import InputText from "./components/InputText";
 import InputCheckbox from "./components/InputCheckbox";
+import Card from "./components/Card";
 
 export default function App() {
   return (
@@ -51,6 +52,10 @@ export default function App() {
 
       <div>
         <InputCheckbox />
+      </div>
+
+      <div>
+        <Card size="md">Olá Mundo</Card>
       </div>
     </div>
   );
